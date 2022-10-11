50568baac9a0a9eb1353cd2e3d941f037a238391
Eric Joel Odoi <sperhit.1@gmail.com>
2022-10-11 10:49:57 +0000

    changed text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome ton my New App!!!</Text>
+      <Text>Welcome to my New APP!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
